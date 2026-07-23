6733313aed1a10f2a24186602b9b4f30ea34c165
Piotr Machocki <piotrmachocki1@gmail.com>
2026-07-23 14:00:39 +0200

    Time: 0 ms (100%), Space: 19.2 MB (64.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,60 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        paren = 0
-        curly = 0
-        square = 0
+        matching = {")": "(", "}": "{", "]": "["}
 
-        match_up = {"(" : ")", "{" : "}", "[" : "]"}
+        stack = []
 
-        LIFO = []
-
-        for i in range(len(s)):
-            
-            if s[i] in match_up:
-
-                if i + 1 < len(s) and s[i+1] in match_up.values() and match_up[s[i]] != s[i+1]:
-                    return False
-
-                elif s[i] == "(":
-                    paren += 1
-                    LIFO.append("(")
-                
-                elif s[i] == "{":
-                    curly += 1
-                    LIFO.append("{")
-
-                elif s[i] == "[":
-                    square += 1
-                    LIFO.append("[")
-
-            
+        for char in s:
+            if char not in matching:
+                stack.append(char)
             else:
-                if len(LIFO) > 0 and match_up[LIFO[(len(LIFO) - 1)]] == s[i]:
-
-                    LIFO.pop()
-
-                    if s[i] == ")":
-                        if paren:
-                            paren -= 1
-                        else:
-                            return False
-
-                    if s[i] == "}":
-                        if curly:
-                            curly -= 1
-                        else:
-                            return False
-                    
-                    if s[i] == "]":
-                        if square:
-                            square -= 1
-                        else:
-                            return False
+                if stack and matching[char] == stack[-1]:
+                    stack.pop()
                 else:
                     return False
 
-        if paren + curly + square == 0:
+        if not stack:
             return True
         else:
             return False
