6ac5876ff665281cadd170d9d85dd2b94ec35d2d
Piotr Machocki <piotrmachocki1@gmail.com>
2026-08-07 18:46:34 +0200

    Time: 3 ms (17.97%), Space: 19.8 MB (8.15%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,45 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        
-        j = 0
 
-        for i in range(len(haystack)):
-            
-            while i < len(haystack) and j < len(needle) and haystack[i] == needle[j]:
-                j += 1
+        # KMP Algorithm
+
+        # Computing LPS Array
+
+        LPS = [0] * len(needle)
+        i = 1
+        prevLPS = 0
+
+        while i < len(needle):
+
+            if needle[i] == needle[prevLPS]:
+                prevLPS += 1
+                LPS[i] = prevLPS
                 i += 1
-            
-            if j == len(needle):
-                return i - j
             else:
-                j = 0
+                if prevLPS:
+                    prevLPS = LPS[prevLPS - 1]
+                else:
+                    LPS[i] = 0
+                    i += 1
+
+        # traversing the text (haystack)
+
+        h_ptr = 0
+        n_ptr = 0
+
+        while h_ptr < len(haystack):
+
+            if haystack[h_ptr] == needle[n_ptr]:
+                h_ptr += 1
+                n_ptr += 1
+
+                if n_ptr == len(needle):
+                    return h_ptr - len(needle)
+            else:
+                if n_ptr:
+                    n_ptr = LPS[n_ptr - 1]
+                else:
+                    h_ptr += 1
 
         return -1
+
